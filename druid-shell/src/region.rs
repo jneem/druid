3ca8ea2caff90d8f0533f634ae8b4479e8f244fb
Joe Neeman <joeneeman@gmail.com>
2020-06-24 16:27:56 -0500

    More cleanup and docs.

--- druid-shell/src/region.rs
+++ druid-shell/src/region.rs
@@ -1,31 +1,38 @@
 use kurbo::{Rect, Vec2};
 
+/// A union of rectangles, useful for describing an area that needs to be repainted.
 #[derive(Clone, Debug)]
 pub struct Region {
     rects: Vec<Rect>,
 }
 
 impl Region {
+    /// The empty region.
     pub const EMPTY: Region = Region { rects: Vec::new() };
 
+    /// Returns the collection of rectangles making up this region.
     #[inline]
     pub fn rects(&self) -> &[Rect] {
         &self.rects
     }
 
+    /// Adds a rectangle to this region.
     pub fn add_rect(&mut self, rect: Rect) {
         self.rects.push(rect);
     }
 
+    /// Replaces this region with a single rectangle.
     pub fn set_rect(&mut self, rect: Rect) {
         self.clear();
         self.rects.push(rect);
     }
 
+    /// Sets this region to the empty region.
     pub fn clear(&mut self) {
         self.rects.clear();
     }
 
+    /// Returns a rectangle containing this region.
     pub fn bounding_box(&self) -> Rect {
         if self.rects.is_empty() {
             Rect::ZERO
